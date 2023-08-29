64efca40aaa75d095f6e828617e14bdb0d85722f
Daniel Peukert <daniel@peukert.cc>
2023-08-30 01:02:55 +0200

    test: add content-type tests

--- internal/doc/doc_test.go
+++ internal/doc/doc_test.go
@@ -10,18 +10,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var testUrls = []string{
-	"/rpc/swagger",
-	"/rpc/swagger/",
-	"/api/swagger",
-	"/api/swagger/",
-	"/admin/swagger",
-	"/admin/swagger/",
-	"/nonsense/swagger/",
-	"/rpc/swagger/openapi.json",
-	"/api/swagger/openapi.json",
-	"/admin/swagger/openapi.json",
-	"/rpc/olddoc.html",
+var testUrls = map[string]string{
+	"/rpc/swagger": "text/html; charset=utf-8",
+	"/rpc/swagger/": "text/html; charset=utf-8",
+	"/api/swagger": "text/html; charset=utf-8",
+	"/api/swagger/": "text/html; charset=utf-8",
+	"/admin/swagger": "text/html; charset=utf-8",
+	"/admin/swagger/": "text/html; charset=utf-8",
+	"/nonsense/swagger/": "text/plain; charset=utf-8",
+	"/rpc/swagger/openapi.json": "application/json",
+	"/api/swagger/openapi.json": "application/json",
+	"/admin/swagger/openapi.json": "application/json",
+	"/rpc/olddoc.html": "text/html; charset=utf-8",
 }
 
 func TestSwagger(t *testing.T) {
@@ -39,13 +39,14 @@ func TestSwagger(t *testing.T) {
 	r.HandleFunc("/admin/swagger/openapi.json", SpecAdminHandler)
 	r.HandleFunc("/rpc/olddoc.html", SpecOldHandler)
 
-	for _, url := range testUrls {
+	for url, contentType := range testUrls {
 		rr := httptest.NewRecorder()
 		req, err := http.NewRequest("GET", url, nil)
 		assert.Nil(t, err, "Could not create GET request")
 
 		r.ServeHTTP(rr, req)
 
+		assert.Equal(t, contentType, rr.Result().Header.Get("Content-Type"), "Wrong Content-Type header")
 		if strings.Contains(url, "nonsense") {
 			assert.Equal(t, http.StatusNotFound, rr.Result().StatusCode, "Wrong http status code", url)
 			assert.Equal(t, "404 page not found\n", rr.Body.String(), "wrong body text", url)
